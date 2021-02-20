1265ed5ea67e0afcc09cad7b02a2f52f02ef9f7e
Dawid Wróbel <me@dawidwrobel.com>
2021-02-20 15:05:47 -0500

    Refactor ReportTable to use KMMUrl

--- kmymoney/plugins/views/reports/core/reporttable.cpp
+++ kmymoney/plugins/views/reports/core/reporttable.cpp
@@ -1,5 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2007-2018 Thomas Baumgart <tbaumgart@kde.org>
+    SPDX-FileCopyrightText: 2021 Dawid Wróbel <me@dawidwrobel.com>
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
@@ -18,6 +19,7 @@
 // ----------------------------------------------------------------------------
 // Project Includes
 
+#include "kmmurl.h"
 #include "kmymoneysettings.h"
 #include "kmymoneyutils.h"
 #include "mymoneyfile.h"
@@ -89,15 +91,7 @@ QString reports::ReportTable::renderHeader(const QString& title, const QByteArra
     }
   } else {
 
-    auto scheme = QStringLiteral("file");
-
-    if (cssfilename.at(0) == ':') {
-      cssfilename.remove(0, 1);
-      scheme = QStringLiteral("qrc");
-    }
-
-    QUrl cssUrl = cssUrl.fromLocalFile(cssfilename);
-    cssUrl.setScheme(scheme);
+    QUrl cssUrl = cssUrl.fromUserInput(cssfilename);
 
     // do not include css inline instead use a link to the css file
     header += "\n<link rel=\"stylesheet\" type=\"text/css\" href=\""
