03081bce57f55bac9ff6a813102f3d60acc3bab6
luigi <luigi.mina@gmail.com>
2021-11-07 12:50:32 +0000

    corretto script italiano in stable

--- italiano.sh
+++ italiano.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-svn co svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/it/messages/kmymoney kmymoney_it
+svn co svn://anonsvn.kde.org/home/kde/branches/stable/l10n-kf5/it/messages/kmymoney kmymoney_it
 cd kmymoney_it
 sudo msgfmt kmymoney.po -o /usr/share/locale/it/LC_MESSAGES/kmymoney.mo
 cd ..
