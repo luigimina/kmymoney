aee66b864323de9741bd66ecfd721359470093c4
luigi <luigi.mina@gmail.com>
2022-01-31 16:52:17 +0000

    install local from container

---
+++ build_in_prod.sh
@@ -0,0 +1,4 @@
+mkdir build_prod
+cd build_prod
+cmake .. -DCMAKE_INSTALL_PREFIX=/workspaces/usr/
+make -j4
---
+++ italiano_prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+svn co svn://anonsvn.kde.org/home/kde/branches/stable/l10n-kf5/it/messages/kmymoney kmymoney_it
+cd kmymoney_it
+sudo msgfmt kmymoney.po -o /workspaces/usr/share/locale/it/LC_MESSAGES/kmymoney.mo
+cd ..
+sudo rm -r kmymoney_it
