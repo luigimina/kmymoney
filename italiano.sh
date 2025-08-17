3336c75e1c171b70bbb4392e57bbebce04956f3c
Luigi <luigi.mina@gmail.com>
2025-08-17 15:48:34 +0000

    ita

--- italiano.sh
+++ italiano.sh
@@ -3,4 +3,4 @@ svn co svn://anonsvn.kde.org/home/kde/branches/stable/l10n-kf5/it/messages/kmymo
 cd kmymoney_it
 sudo msgfmt kmymoney.po -o /usr/share/locale/it/LC_MESSAGES/kmymoney.mo
 cd ..
-sudo rm -r 
+sudo rm -r kmymoney_it
